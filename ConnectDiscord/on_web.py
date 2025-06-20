a64de09f5485bf8573fd90c2c4bea360779b73b9
ISrbz <159147342+ISrbz@users.noreply.github.com>
2025-06-20 16:00:16 +0300

    now returns json response

--- ConnectDiscord/on_web.py
+++ ConnectDiscord/on_web.py
@@ -3,7 +3,7 @@
 # @author ISrbz 
 # @brief Deals with flask.
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from hist import viewHist, viewPrev
 from textHook import sendMessage
 
@@ -21,24 +21,38 @@ if __name__ == "__main__":
 
 
 ##show a previous message
-@app.route('/<server>/<channel>/history')
+#@app.route('/<server>/<channel>/history')
 def msg_prev(server, channel):
     num = request.args['num']
     info, message = viewHist(server, channel, num)
-    return render_template('basic_display_one.html', info=info, content=message)
-
+    data = {'info': info, 'message': message}
+    return jsonify(data)
 ##show current message in this channel
-@app.route('/<server>/<channel>/')
+#@app.route('/<server>/<channel>/')
 def msg_last(server, channel):
     info, message = viewPrev(server, channel)
-    return render_template('basic_display_one.html', info=info, content=message)
+    data = {'info': info, 'message': message}
+    return jsonify(data)
+    #return render_template('basic_display_one.html', info=info, content=message)
 
 ##send a message to this channel
-@app.route('/<server>/<channel>/', methods=['POST'])
-@app.route('/<server>/<channel>/history', methods=['POST'])
+@app.route('/<server>/<channel>/')
 def send_msg(server, channel):
-    message = request.form['text']
-    sendMessage(message, server, channel)
-    new_url = '/' + server + '/' + channel
-    return redirect(new_url, code=302)
+    message = request.args.get('text')
+    if message is None:
+        return msg_last(server, channel)
+    else:
+        sendMessage(message, server, channel)
+        new_url = '/' + server + '/' + channel
+        return redirect(new_url, code=302)
+    
+@app.route('/<server>/<channel>/history')
+def send_msg_hist(server, channel):
+    message = request.args.get('text')
+    if message is None:
+        return msg_prev(server, channel)
+    else:
+        sendMessage(message, server, channel)
+        new_url = '/' + server + '/' + channel
+        return redirect(new_url, code=302)
 
